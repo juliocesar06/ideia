368781ba162ea22ba02fd0f46d4ee5a98d8c3d85
juliocesar06 <julio06cesar11@gmail.com>
2021-11-14 12:04:48 -0300

    one pag

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+function Home() {
+  return (
+    <div>
+      <p>Home</p>
+      <Contador />
+    </div>
+  );
+}
+
+function Contador() {
+  const [contador, setContador] = useState(1);
+
+  function adcionarContador() {
+    setContador(contador + 1);
+  }
+
+  return (
+    <div>
+      <div>{contador}</div>
+      <button onClick={adcionarContador}>Adcionar</button>
+    </div>
+  );
+}
+
+export default Home;
